f4e61872fb1211af948ecaee26c34f417cefef30
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2020-12-02 12:29:41 -0500

    finish prototype

--- script.js
+++ script.js
@@ -23,6 +23,246 @@ $("#remove-event-2").click(function () {
    $("#list2").toggleClass("d-none");
 });
 
+// $(".cal-box").hover(function () {
+//    $(".edit-banner").toggleClass("d-none");
+// });
+
+$("#cal-box-1").hover(function () {
+   $("#cal-banner-1").toggleClass("d-none");
+});
+
+$("#cal-box-2").hover(function () {
+   $("#cal-banner-2").toggleClass("d-none");
+});
+
+$("#cal-box-3").hover(function () {
+   $("#cal-banner-3").toggleClass("d-none");
+});
+
+$("#cal-box-4").hover(function () {
+   $("#cal-banner-4").toggleClass("d-none");
+});
+
+$("#cal-box-5").hover(function () {
+   $("#cal-banner-5").toggleClass("d-none");
+});
+
+$("#cal-box-6").hover(function () {
+   $("#cal-banner-6").toggleClass("d-none");
+});
+
+$("#cal-box-7").hover(function () {
+   $("#cal-banner-7").toggleClass("d-none");
+});
+
+$("#cal-box-8").hover(function () {
+   $("#cal-banner-8").toggleClass("d-none");
+});
+
+$("#cal-box-9").hover(function () {
+   $("#cal-banner-9").toggleClass("d-none");
+});
+
+$("#cal-box-10").hover(function () {
+   $("#cal-banner-10").toggleClass("d-none");
+});
+
+$("#cal-box-11").hover(function () {
+   $("#cal-banner-11").toggleClass("d-none");
+});
+
+$("#cal-box-12").hover(function () {
+   $("#cal-banner-12").toggleClass("d-none");
+});
+
+$("#cal-box-13").hover(function () {
+   $("#cal-banner-13").toggleClass("d-none");
+});
+
+$("#cal-box-14").hover(function () {
+   $("#cal-banner-14").toggleClass("d-none");
+});
+
+$("#cal-box-15").hover(function () {
+   $("#cal-banner-15").toggleClass("d-none");
+});
+
+$("#cal-box-16").hover(function () {
+   $("#cal-banner-16").toggleClass("d-none");
+});
+
+$("#cal-box-17").hover(function () {
+   $("#cal-banner-17").toggleClass("d-none");
+});
+
+$("#cal-box-18").hover(function () {
+   $("#cal-banner-18").toggleClass("d-none");
+});
+
+$("#cal-box-19").hover(function () {
+   $("#cal-banner-19").toggleClass("d-none");
+});
+
+$("#cal-box-20").hover(function () {
+   $("#cal-banner-20").toggleClass("d-none");
+});
+
+$("#cal-box-21").hover(function () {
+   $("#cal-banner-21").toggleClass("d-none");
+});
+
+$("#cal-box-22").hover(function () {
+   $("#cal-banner-22").toggleClass("d-none");
+});
+
+$("#cal-box-23").hover(function () {
+   $("#cal-banner-23").toggleClass("d-none");
+});
+
+$("#cal-box-24").hover(function () {
+   $("#cal-banner-24").toggleClass("d-none");
+});
+
+$("#cal-box-25").hover(function () {
+   $("#cal-banner-25").toggleClass("d-none");
+});
+
+$("#cal-box-26").hover(function () {
+   $("#cal-banner-26").toggleClass("d-none");
+});
+
+$("#cal-box-27").hover(function () {
+   $("#cal-banner-27").toggleClass("d-none");
+});
+
+$("#cal-box-28").hover(function () {
+   $("#cal-banner-28").toggleClass("d-none");
+});
+
+$("#cal-box-29").hover(function () {
+   $("#cal-banner-29").toggleClass("d-none");
+});
+
+$("#cal-box-30").hover(function () {
+   $("#cal-banner-30").toggleClass("d-none");
+});
+
+$("#cal-box-31").hover(function () {
+   $("#cal-banner-31").toggleClass("d-none");
+});
+
+$("#cal-box-32").hover(function () {
+   $("#cal-banner-32").toggleClass("d-none");
+});
+
+$("#cal-box-33").hover(function () {
+   $("#cal-banner-33").toggleClass("d-none");
+});
+
+$("#cal-box-34").hover(function () {
+   $("#cal-banner-34").toggleClass("d-none");
+});
+
+$("#cal-box-35").hover(function () {
+   $("#cal-banner-35").toggleClass("d-none");
+});
+
+$("#cal-box-36").hover(function () {
+   $("#cal-banner-36").toggleClass("d-none");
+});
+
+$("#cal-box-37").hover(function () {
+   $("#cal-banner-37").toggleClass("d-none");
+});
+
+$("#cal-box-38").hover(function () {
+   $("#cal-banner-38").toggleClass("d-none");
+});
+
+$("#cal-box-39").hover(function () {
+   $("#cal-banner-39").toggleClass("d-none");
+});
+
+$("#cal-box-1-2").hover(function () {
+   $("#cal-banner-1-2").toggleClass("d-none");
+});
+
+$("#cal-box-2-2").hover(function () {
+   $("#cal-banner-2-2").toggleClass("d-none");
+});
+
+$("#cal-box-3-2").hover(function () {
+   $("#cal-banner-3-2").toggleClass("d-none");
+});
+
+$("#cal-box-4-2").hover(function () {
+   $("#cal-banner-4-2").toggleClass("d-none");
+});
+
+$("#cal-box-5-2").hover(function () {
+   $("#cal-banner-5-2").toggleClass("d-none");
+});
+
+$("#cal-box-6-2").hover(function () {
+   $("#cal-banner-6-2").toggleClass("d-none");
+});
+
+$("#cal-box-7-2").hover(function () {
+   $("#cal-banner-7-2").toggleClass("d-none");
+});
+
+$("#cal-box-8-2").hover(function () {
+   $("#cal-banner-8-2").toggleClass("d-none");
+});
+
+$("#cal-box-9-2").hover(function () {
+   $("#cal-banner-9-2").toggleClass("d-none");
+});
+
+$("#cal-box-10-2").hover(function () {
+   $("#cal-banner-10-2").toggleClass("d-none");
+});
+
+$("#cal-box-1-3").hover(function () {
+   $("#cal-banner-1-3").toggleClass("d-none");
+});
+
+$("#cal-box-2-3").hover(function () {
+   $("#cal-banner-2-3").toggleClass("d-none");
+});
+
+$("#cal-box-3-3").hover(function () {
+   $("#cal-banner-3-3").toggleClass("d-none");
+});
+
+$("#cal-box-4-3").hover(function () {
+   $("#cal-banner-4-3").toggleClass("d-none");
+});
+
+$("#cal-box-5-3").hover(function () {
+   $("#cal-banner-5-3").toggleClass("d-none");
+});
+
+$("#cal-box-6-3").hover(function () {
+   $("#cal-banner-6-3").toggleClass("d-none");
+});
+
+$("#cal-box-7-3").hover(function () {
+   $("#cal-banner-7-3").toggleClass("d-none");
+});
+
+$("#cal-box-8-3").hover(function () {
+   $("#cal-banner-8-3").toggleClass("d-none");
+});
+
+$("#cal-box-9-3").hover(function () {
+   $("#cal-banner-9-3").toggleClass("d-none");
+});
+
+$("#cal-box-10-3").hover(function () {
+   $("#cal-banner-10-3").toggleClass("d-none");
+});
+
 // $("#lets-go").click(function () {
 //    $("#sign-up-card").toggleClass("d-none");
 //    $("#intro-card").toggleClass("d-none");
