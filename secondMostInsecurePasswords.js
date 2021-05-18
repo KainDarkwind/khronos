deee4718f0d2122d9a4013422f4053232e2deca5
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2021-05-18 17:00:13 -0400

    Completely Destroyed

--- secondMostInsecurePasswords.js
+++
@@ -1,9 +0,0 @@
-const secondMostInsecurePasswords = [
-   ["mypassword", true],
-   ["password1", true],
-   ["zxcvbnm", true],
-   [1234567890, false],
-   ["gosteelers", true],
-   [51015202530, false],
-   ["Password1", true],
-];
